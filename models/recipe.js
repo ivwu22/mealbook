44549cd091c5c54c0e4d14c2b67127418c82ed9f
ivwu22 <ivwu22@gmail.com>
2021-02-22 22:31:57 -0500

    adds database and associations (minus calendar), pair programmed with cindy

---
+++ models/recipe.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class recipe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.recipe.hasMany(models.instruction)
+      models.recipe.hasMany(models.ingredient)
+      models.recipe.belongsToMany(models.user, {through: favorites})
+    }
+  };
+  recipe.init({
+    name: DataTypes.STRING,
+    picture: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'recipe',
+  });
+  return recipe;
+};

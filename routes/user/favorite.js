38078ab73f8dcf2bc37a7a3dcdb11e16804bf068
muichc <cindy.ch.mui@gmail.com>
2021-02-24 11:52:22 -0800

    changed some favorites get route things

--- routes/user/favorite.js
+++ routes/user/favorite.js
@@ -1,9 +1,10 @@
 //route to show favorite recipes
 const express = require('express');
 const db = require('../../models');
+const isLoggedIn = require('../../middleware/isLoggedIn')
 const router = express.Router();
 
-router.get('/favorite', (req, res) => {
+router.get('/', (req, res) => {
    res.render('user/favorites.ejs');
 })
 
--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/profile', isLoggedIn, (req, res) => {
 
 app.use('/auth', require('./routes/auth'));
 app.use('/recipe', require('./routes/recipe'));
-app.use('/user/favorite', require('./routes/user/favorite.js'));
+app.use('/user/favorites', require('./routes/user/favorite.js'));
 app.use('/user/calendar', require('./routes/user/calendar.js'));
 app.use('/user/dashboard', require('./routes/user/dashboard.js'));
 

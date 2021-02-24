4436ec6a904395528adb36808f9d7214105c93b8
ivwu22 <ivwu22@gmail.com>
2021-02-24 13:19:15 -0500

    seeds database; pair programmed with Cindy

--- models/favorites.js
+++ models/favorites.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.favorites.hasMany(models.calendar)
     }
   };
   favorites.init({

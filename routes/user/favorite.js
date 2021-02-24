e27d5ea9db26d3ec8d46ffa5b633b51e197f714c
muichc <cindy.ch.mui@gmail.com>
2021-02-24 13:27:25 -0800

    finished get routes and view for favorite recipes page

--- routes/user/favorite.js
+++ routes/user/favorite.js
@@ -4,8 +4,17 @@ const db = require('../../models');
 const isLoggedIn = require('../../middleware/isLoggedIn')
 const router = express.Router();
 
-router.get('/', (req, res) => {
-   res.render('user/favorites.ejs');
+router.get('/', isLoggedIn, (req, res) => {
+   console.log("req.user.id is ", req.user.id);
+   db.user.findOne({
+      where: {
+         id: req.user.id
+      }, include: [db.recipe]
+   }).then((foundUser)=> {
+      console.log(foundUser.recipes);
+      res.render('user/favorites.ejs', {favoritesList:foundUser.recipes, isFavorite: true})
+      
+   })
 })
 
 module.exports = router;

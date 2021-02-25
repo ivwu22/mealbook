45597f3fa61ed3c9ae32248fd76101cda7a02296
muichc <cindy.ch.mui@gmail.com>
2021-02-24 16:54:36 -0800

    some progress with dashboard

--- routes/recipe.js
+++ routes/recipe.js
@@ -15,13 +15,4 @@ router.get('/', (req,res) => {
     })
 })
 
-router.get('/', (req,res) => {
-    db.recipe.findAll()
-    .then((recipes) => {
-        res.render('recipe/explore', {recipes:recipes})
-    }).catch((error) => {
-        res.status(404).render('main/404')
-    })
-})
-
 module.exports = router;
--- routes/user/dashboard.js
+++ routes/user/dashboard.js
@@ -1,8 +1,42 @@
 //route to show all the components of the dashboard when user signs in
 const express = require('express');
 const db = require('../../models');
+const isLoggedIn = require('../../middleware/isLoggedIn')
 const router = express.Router();
-router.get('/dashboard', (req, res) => {
-    res.send('hello')
+
+router.get('/', isLoggedIn, (req, res) => {
+    let randomRecipes = [];
+    db.recipe.findAll().then(function(foundRecipes){
+        const randomNumbers= getTwoRandom(foundRecipes.length);
+        for (let item in randomNumbers) {
+            randomRecipes.push(foundRecipes[randomNumbers[item]]);
+        }
+    })
+    db.user.findOne({
+        where: {
+            id: req.user.id
+        }, include:[db.recipe]
+    }).then(function(foundUser){
+        res.render('user/dashboard', {userFavorites:foundUser.recipes, recipes:randomRecipes})
+    })
 })
+
+
+
+function getRandomInt(min, max){
+    min = Math.ceil(min);
+    max= Math.floor(max);
+    return Math.floor(Math.random()*(max-min)+min);
+}
+function getTwoRandom(max){
+    const randomNumbers= [];
+    randomNumbers.push(getRandomInt(0,max))
+    randomNumbers.push(getRandomInt(0,max))
+    while(randomNumbers[0]===randomNumbers[1]){
+        randomNumbers[1]=getRandomInt(0,max)
+    }
+    return randomNumbers;
+}
+
+
 module.exports = router;

7cb229546f5f143d5f1456a10dc590fb89c2d057
muichc <cindy.ch.mui@gmail.com>
2021-02-26 20:02:43 -0800

    Work with dashboard, explore search functions, work in progress

--- models/favorites.js
+++ models/favorites.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.favorites.belongsTo(models.user)
+      models.favorites.belongsTo(models.recipe)
     }
   };
   favorites.init({
